02382fa5747e8d26127d11cbd59af12d8044ae79
ibnzterrell <terrell.ibanez@gmail.com>
2022-06-21 17:08:13 -0700

    Fix double nested module name

--- libraryJS/src/index.js
+++ libraryJS/src/index.js
@@ -1,9 +1,4 @@
 import {Analyzer} from "./analyzer"
 import {Annotator} from "./annotator"
 
-let TwinPeaks = {};
-
-TwinPeaks.Analyzer = Analyzer
-TwinPeaks.Annotator = Annotator
-
-export {TwinPeaks}
+export {Analyzer, Annotator}
