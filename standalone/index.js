25cd0d720db56f31ec081d5c196eb836cffcf630
ibnzterrell <terrell.ibanez@gmail.com>
2022-06-22 09:58:10 -0700

    Separate chart JS code from HTML

--- standalone/index.js
+++ standalone/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const path = require('path');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Standalone Annotator listeining on port ${port}`)
+    console.log(`Standalone Annotator listening on port ${port}`);
 })
