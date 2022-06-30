f0bc679c7130500dfef435c5576711ed698e404d
ibnzterrell <terrell.ibanez@gmail.com>
2022-06-30 09:54:35 -0700

    Fix build script to create dist folder

--- standalone/build.js
+++ standalone/build.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 
+const dir = './public/dist';
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
 fs.copyFile('../libraryJS/dist/index_bundle.umd.js', './public/dist/twinpeaks.js', () => {});
