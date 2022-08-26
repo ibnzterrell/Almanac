a1ece60d4fc5c84d0a5170083f2939c4f25d52e6
ibnzterrell <terrell.ibanez@gmail.com>
2022-08-26 12:09:01 -0700

    Implement range clustering

--- annotation/annotator-api/api_container/services/headline_service.py
+++ annotation/annotator-api/api_container/services/headline_service.py
@@ -102,13 +102,12 @@ def headline_point_query(db, pipelines, data: list[dict], params: dict, options:
 
 
 def headline_range_query(db, pipelines, data: list[dict], params: dict, options: dict):
-    granularity = params["granularity"]
-    dateField = params["dateField"]
+    ranges = params["ranges"]
     query = params["query"]
 
     data = pd.DataFrame.from_records(data)
 
-    return headline_point_cluster(db, pipelines, data, granularity, dateField, query, options)
+    return headline_range_cluster(db, pipelines, data, ranges, query, options)
 
 
 def findPointClusters(df, pipelines, events, dateField, granularity, options):
@@ -231,6 +230,175 @@ def headline_point_cluster(db, pipes, events, granularity, dateField, query, opt
                                       events, dateField, granularity, options)
 
     df["date_period"] = df["date_period"].astype(str)
+    # print(df)
+
+    df = df.loc[:, ~df.columns.isin(["doc"])]
+
+    headlines = json.loads(
+        df[df["alternate"] == False].to_json(orient="records"))
+
+    res_data = {"headlines": headlines}
+
+    if (options["alternates"]):
+        res_data["alternates"] = json.loads(
+            df[df["alternate"] == True].to_json(orient="records"))
+
+    if (options["topK"]):
+        res_data["topK"] = dptopKs
+
+    return res_data
+
+
+def findRangeClusters(df, pipelines, ranges, options):
+    nlp = getNLP(pipelines, options)
+
+    df["date_p"] = pd.to_datetime(df['pub_date'])
+
+    rangemasks = [(df['date_p'] >= r.start & df['date_p'] <= r.end)
+                  for r in range]
+
+    rdfs = []
+    for r, rm in zip(ranges, rangemasks):
+        rdf = df.loc[rm]
+        rdf["date_range"] = r.start.strftime(
+            "%Y-%m-%d") + " - " + r.end.strftime("%Y-%m-%d")
+
+    df = pd.concat(rdfs)
+
+    df = df[["pub_date", "date_range",
+             "main_headline", "lead_paragraph", "web_url"]]
+
+    date_ranges = pd.unique(df["date_range"])
+
+    match options["scoringSpace"]:
+        case HeadlineSearch.headline:
+            df["doc"] = df["main_headline"]
+        case HeadlineSearch.headlinewithlead:
+            df["doc"] = df["main_headline"] + " " + df["lead_paragraph"]
+
+    df["doc"] = df["doc"].str.lower()
+    df["doc"] = list(nlp.pipe(df["doc"]))
+
+    all_words = [token.lemma_ for doc in df["doc"]
+                 for token in doc if wordFilter(token, options["alphaFilter"])]
+
+    dictionary = sorted(set(all_words))
+
+    # Term Frequencies
+    fs = []
+    trfs = []
+    trfirfs = []
+
+    if (options["singleDocumentFilter"]):
+        wordsets = []
+        for dr in date_ranges:
+            docwordsets = []
+            for doc in df[df["date_range"] == dr]["doc"]:
+                docwordset = set(
+                    [token.lemma_ for token in doc if wordFilter(token, options["alphaFilter"])])
+                docwordsets.append(docwordset)
+            rangewords = [word for docset in docwordsets for word in docset]
+            wordsets.append(set(rangewords))
+        dictionary = sorted(
+            set([word for wordset in wordsets for word in wordset]))
+
+    # Calculate Range Term Frequencies
+    for dr in date_ranges:
+        words = []
+        if (options["booleanFrequencies"]):
+            docwordsets = []
+            for doc in df[df["date_range"] == dr]["doc"]:
+                docwordset = set(
+                    [token.lemma_ for token in doc if wordFilter(token, options["alphaFilter"])])
+                docwordsets.append(docwordset)
+            words = [word for docset in docwordsets for word in docset]
+        else:
+            words = [token.lemma_ for doc in df[df["date_range"] == dr]["doc"]
+                     for token in doc if wordFilter(token, options["alphaFilter"])]
+        fc = Counter(words)
+
+        f = {w: fc.get(w, 0) for w in dictionary}
+        fs.append(f)
+        trf = {w: f.get(w, 0) / (len(words) + 1) for w in dictionary}
+        trfs.append(trf)
+
+    N = len(date_ranges)
+
+    # Calculate Range Frequencies
+    rf = {w: sum([1 if (f.get(w, 0) > 0) else 0 for f in fs])
+          for w in dictionary}
+
+    # Calculate Inverse Range Frequencies
+    irf = {w: math.log(N / (rf[w])) for w in dictionary}
+
+    # Calculate TRF-IRFs
+    for trf in trfs:
+        trfirf = {w: trf[w] * irf[w] for w in dictionary}
+
+        if (options["decayWeighting"]):
+            # Apply Exponential Decay Weighting
+            trfsorted = sorted(
+                trfirf.items(), key=lambda kv: kv[1], reverse=True)
+            trfirf = {k: v * pow(0.80, i)
+                      for (i, (k, v)) in enumerate(trfsorted, 0)}
+
+        trfirfs.append(trfirf)
+
+    topks = []
+
+    for (f, trf, trfirf) in zip(fs, trfs, trfirfs):
+        topk = heapq.nlargest(25, trf, key=trfirf.get)
+        topk = {w: {"f": f.get(w, 0), "trf": trf.get(w, 0), "irf": irf.get(
+            w, 0), "rf": rf.get(w, 0), "trfirf": trfirf.get(w, 0)} for w in topk}
+        topks.append(topk)
+
+    drtopKs = {str(dr): topktrfirf for (dr, topktrfirf)
+               in zip(date_ranges, topks)}
+
+    trfDateLookup = {dr: trf for (dr, trf) in zip(date_ranges, trfs)}
+
+    df["score"] = scoreDocs(
+        df["doc"], df["date_range"], trfDateLookup, options)
+
+    df = df.sort_values(by="score", ascending=False)
+
+    df["alternate"] = df.duplicated(subset=["date_range"], keep="first")
+
+    return (df, drtopKs)
+
+
+def headline_range_cluster(db, pipes, events, granularity, dateField, query, options):
+    df = textEventQuery(db, query, options)
+
+    (df, dptopKs) = findPointClusters(df, pipes,
+                                      events, dateField, granularity, options)
+
+    df["date_period"] = df["date_period"].astype(str)
+    # print(df)
+
+    df = df.loc[:, ~df.columns.isin(["doc"])]
+
+    headlines = json.loads(
+        df[df["alternate"] == False].to_json(orient="records"))
+
+    res_data = {"headlines": headlines}
+
+    if (options["alternates"]):
+        res_data["alternates"] = json.loads(
+            df[df["alternate"] == True].to_json(orient="records"))
+
+    if (options["topK"]):
+        res_data["topK"] = dptopKs
+
+    return res_data
+
+
+def headline_range_cluster(db, pipes, ranges, query, options):
+    df = textEventQuery(db, query, options)
+
+    (df, dptopKs) = findRangeClusters(df, pipes, ranges, options)
+
+    df["date_range"] = df["date_range"].astype(str)
     # print(df)
 
     df = df.loc[:, ~df.columns.isin(["doc"])]
